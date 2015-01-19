09b986415d65cf598ea39906c0ece1b5c3eeb772
rosspalmer <rossbpalmer@gmail.com>
2015-01-19 16:32:54 -0700

    added trades to price history functions

--- modules/db.py
+++ modules/db.py
@@ -1,11 +1,12 @@
 import auth
 import api
 import sql
+from tools import date_index, time_series, seconds
 
 from pandas import DataFrame
 from pandas import read_sql_query
-from pandas.tseries.tools import to_datetime
 from sqlalchemy import create_engine,  MetaData
+from sqlalchemy.sql import select
 
 #|Connect to SQL database and create SQLAlchemy engine and MetaData
 def dbconnect():	
@@ -32,14 +33,6 @@ def add_to_db(df, table):
 	ins = table.insert().prefix_with('IGNORE')	
 	ins.execute(df)	
 
-#|Create datetime index for DataFrame using "timestamp" column
-def date_index(df):
-	date = df['timestamp']
-	date = to_datetime(date, unit='s')
-	df['date'] = date
-	df = df.set_index('date')
-	return df
-
 #|Import BitcoinCharts trade history CSV into SQL database
 #|Before running, place CSV in 'modules' folder and rename file to exchange name
 def import_bcharttrades(exchange):
@@ -59,4 +52,29 @@ def trades_ping(exchange, limit=50):
 	table = sql.tables(meta, 'trades')	
 	df, ping = api.trades(exchange, limit)
 	add_to_db(df, table)
+
+#|Convert and add trade history to SQL database
+def trades_to_pricedb(df, typ, exchange, source):
+	table_name = "pricehistory" + str(typ)
+	eng, conn, meta = dbconnect()
+	table = sql.tables(meta, table_name)
+	ts = time_series(df, seconds(typ=typ))
+	ts['exchange'] = exchange
+	ts['source'] = source	
+	add_to_db(ts, table)
+
+#|Return price history DataFrame using exchange/source filters
+def price_db(typ, exchange, source):
+	table_name = 'pricehistory' + str(typ)
+	eng, conn, meta = dbconnect()
+	tbl = sql.tables(meta, table_name)
+	sel = select([tbl]).where(tbl.c.exchange == exchange)
+	sel = sel.where(tbl.c.source == source)
+	result = conn.execute(sel)
+	result = result.fetchall()
+	df = DataFrame(result, columns=('timestamp','price','amount','high','low',
+					'open','exchange','source'))
+	df = date_index(df)
+	return df
+
 
