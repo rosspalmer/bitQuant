10e48e864354889262a9a405c28af5b9e35943fe
rosspalmer <rossbpalmer@gmail.com>
2015-01-19 16:33:40 -0700

    revised tables

--- modules/sql.py
+++ modules/sql.py
@@ -41,23 +41,23 @@ def dateconv(date):
 
 #|Load SQLAlchemy table into MetaData with option to 'create'
 #|SQL table in database
-def tables(meta, typ, create='no'):	
-	if typ == 'trades':
+def tables(meta, table_name, create='no'):	
+	if table_name == 'trades':
 		table = Table('tds', meta, Column('tid', Integer, primary_key=True),
 			Column('price', Float), Column('amount', Float),
 			Column('type', String(4)), Column('timestamp', Integer),
 			Column('exchange', String(20)))
-	if typ == 'oktrades':
-		table = Table('oktrades', meta, Column('tid', Integer, primary_key=True),
+	if table_name == 'oktrades':
+		table = Table(table_name, meta, Column('tid', Integer, primary_key=True),
 			Column('price', Float), Column('amount', Float),
 			Column('type', String(4)), Column('timestamp', Integer),
 			Column('timestamp_ms', Integer), Column('exchange', String(20)))
-	if typ == 'bcharttrades':
-		table = Table('bcharttrades', meta, Column('timestamp', Integer),
+	if table_name == 'bcharttrades':
+		table = Table(table_name, meta, Column('timestamp', Integer),
 			Column('price', Float), Column('amount', Float),
 			Column('exchange', String(20)))
-	if typ == 'pricehistory':
-		table = Table('pricehistory', meta, Column('timestamp', Integer),
+	if table_name.find('pricehistory') == 0:
+		table = Table(table_name, meta, Column('timestamp', Integer),
 			Column('price', Float), Column('amount', Float),
 			Column('open', Float), Column('high', Float),
 			Column('low', Float), Column('exchange', String(20)),
@@ -65,3 +65,6 @@ def tables(meta, typ, create='no'):
 	if create == 'yes':
 		meta.create_all(eng)
 	return table
+
+
+
