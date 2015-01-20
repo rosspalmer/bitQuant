186460330d2ba9c18ea801a6d8ee39e49e83a4bd
rosspalmer <rossbpalmer@gmail.com>
2015-01-19 22:56:07 -0700

    completed first functional SVM setup

--- modules/svm.py
+++ modules/svm.py
@@ -10,7 +10,7 @@ def binary(df, column_name):
 	return df
 
 def binaryfn(row):
-	rng = 0.007	
+	rng = 0.007
 	if row['binary'] >= rng:
 		result = 1
 	elif row['binary'] <= -rng:
@@ -31,41 +31,48 @@ def plot2D(df):
 	df.plot()
 	plt.show()
 
-def prc_amt_lag(df, plags=3, alags=0):
+def prc_amt_lag(df, plags=1, alags=0):
 	ts = DataFrame(index=df.index)
 	ts['timestamp'] = df['timestamp']	
 	ts['price'] = df['price']
 	ts['amount'] = df['amount']
+	ts['prc_change'] = df['price'].pct_change()*100.0
+	ts['amt_change'] = df['amount'].pct_change()*100.0
 	for i in range(1, plags + 1):
-		header = 'plag%s' % i
-		ts[header] = df['price'].shift(i)
+		header = 'pc_lag%s' % i
+		ts[header] = ts['prc_change'].shift(i)
 	for i in range(1, alags + 1):
-		header = 'alag%s' % i
-		ts[header] = df['amount'].shift(i)
-	ts = ts[max(plags,alags):]
+		header = 'ac_lag%s' % i
+		ts[header] = ts['amt_change'].shift(i)
+	ts = ts[max(plags,alags)+1:]
 	return ts
 
-def zipcol(ts)
+def zipcol(ts):
+	c1 = ts['price'].values
+	c2 = ts['prc_change'].values
+	c3 = ts['pc_lag1'].values
+	c4 = ts['amount'].values
+	c5 = ts['amt_change'].values
+	return zip(c1, c2, c3, c4, c5)
 
 def run():
 
 	df = db.price_db('h', 'bitfinex', 'livetrades')
 	ts = prc_amt_lag(df)
 	ts = binary(ts, 'price')
-	ts1, ts2 = split(ts, 0.75)
-	#price = ts1['price'].values
-	#amount = ts1['amount'].values
-	#data = zip(price, amount)
-	#print('zip')
-	#result = ts1['binary'].values
+	ts1, ts2 = split(ts, 0.50)
+	sample = zipcol(ts1)
+	result = ts1['binary'].values
 
-	#mach = svm.SVC()
-	#mach.fit(data, result)
+	mach = svm.SVC(kernel='poly')
+	print mach
+	mach.fit(sample, result)
 
-	#price = ts2['price'].values
-	#amount = ts2['amount'].values
-	#pred = mach.predict(data)
-	#data = zip(price, amount, pred)
-	#df = DataFrame(data, index=ts2.index, columns=('price','amount','pred'))
+	test = zipcol(ts2)
+	pred = mach.predict(test)
+	pred = Series(pred, index=ts2.index, name='pred')
+	ts2 = ts2.join(pred)
+	print ts2[ts2['pred'] <> 0].drop(['amount','amt_change'], axis=1)
+	print pred.sum()
 
 run()
