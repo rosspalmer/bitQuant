25510a0a940e2becad406006f4d8ac24b934cd8d
rosspalmer <rossbpalmer@gmail.com>
2015-01-29 22:48:36 -0700

    revised table names

--- modules/sql.py
+++ modules/sql.py
@@ -4,17 +4,17 @@ from sqlalchemy import create_engine
 #|Load SQLAlchemy table into MetaData with option to 'create'
 #|SQL table in database
 def tables(meta, table_name):	
-	if table_name == 'trades':
+	if table_name == 'api':
 		tbl = Table('tds', meta, Column('tid', Integer, primary_key=True),
 			Column('price', Float), Column('amount', Float),
 			Column('type', String(4)), Column('timestamp', Integer),
 			Column('exchange', String(20)))
-	if table_name == 'oktrades':
+	if table_name == 'okcoin':
 		tbl = Table(table_name, meta, Column('tid', Integer, primary_key=True),
 			Column('price', Float), Column('amount', Float),
 			Column('type', String(4)), Column('timestamp', Integer),
 			Column('timestamp_ms', Integer), Column('exchange', String(20)))
-	if table_name == 'bchtrades':
+	if table_name == 'bchart':
 		tbl = Table(table_name, meta, Column('timestamp', Integer),
 			Column('price', Float), Column('amount', Float),
 			Column('exchange', String(20)))
