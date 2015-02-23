7b5d806be06fde8d73aa305c30684752af2ffbdc
rosspalmer <rossbpalmer@gmail.com>
2015-02-22 17:12:46 -0700

    0.2.3 :/

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ config = {
 	'author': 'ross palmer',
 	'url':'https://github.com/rosspalmer/bitQuant',
 	'license':'MIT',
-	'version': '0.2.2',
-	'install_requires': ['SQLAlchemy','pandas','numpy','scipy','PyMySQL','sqlite'],
+	'version': '0.2.3',
+	'install_requires': ['SQLAlchemy','pandas','numpy','scipy','PyMySQL','sqlite3'],
 	'packages': ['bitquant'],
 	'scripts': [],
 	'name':'bitquant'
